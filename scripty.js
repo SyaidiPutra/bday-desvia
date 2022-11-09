839d74ef262f470de1f61530e4a55b044ee3f986
SyaidiPutra <syaidiputra29@gmail.com>
2022-11-09 15:08:55 +0800

    first commit

---
+++ scripty.js
@@ -0,0 +1,9 @@
+let bg1 = document.querySelector('#bg1')
+let bg2 = document.querySelector('#bg2')
+let hero = document.querySelector('#hero')
+let guit = document.querySelector('#guit')
+let bata = document.querySelector('#bata')
+
+document.addEventListener('scroll', function () {
+    console.log(scrollY.toString)
+})
