3819654c1638217203d47df25d2d994ef6d76c8b
SyaidiPutra <syaidiputra29@gmail.com>
2022-11-09 16:38:18 +0800

    modified:   scripty.js 	modified:   style.css

--- scripty.js
+++ scripty.js
@@ -4,7 +4,15 @@ let hero = document.querySelector('#hero')
 let guit = document.querySelector('#guit')
 let bata = document.querySelector('#bata')
 
+
 document.addEventListener('scroll', function () {
-    var loc_Y = window.scrollY
-    console.log(loc_Y)
+    var val = window.scrollY
+    
+    bata.style.bottom = -80 - (val * 0.15) + "px"
+    hero.style.bottom = -80 - (val * 0.3) + "px"
+    bg2.style.top = -10 + (val * 0.05) + "px"
+    bg1.style.top = -10 + (val * 0.2) + "px"
+    guit.style.bottom = -80 + (val * 0.2) + "px"
+    guit.style.right = (val * 0.2) + "px"
+
 })
